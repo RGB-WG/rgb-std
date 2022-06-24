3fd394ceb38bc1444dce09e50c59890eb2761866
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-06-24 23:29:23 +0200

    PSBT RGB API trait

--- src/psbt.rs
+++ src/psbt.rs
@@ -11,11 +11,11 @@
 
 // TODO: Implement state transition ops for PSBT
 
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 use bitcoin::psbt::raw::ProprietaryKey;
 use commit_verify::TaggedHash;
-use rgb_core::{ContractId, NodeId, Transition};
+use rgb_core::{ContractId, NodeId, Transition, TransitionBundle};
 use wallet::psbt;
 use wallet::psbt::Psbt;
 
@@ -28,8 +28,8 @@ pub const PSBT_RGB_PREFIX: &[u8] = b"RGB";
 pub const PSBT_GLOBAL_RGB_CONTRACT: u8 = 0x00;
 /// Proprietary key subtype for storing RGB state transition in global map.
 pub const PSBT_GLOBAL_RGB_TRANSITION: u8 = 0x01;
-/// Proprietary key subtype for storing RGB node id in input map.
-pub const PSBT_IN_RGB_NODE_ID: u8 = 0x03;
+/// Proprietary key subtype for storing RGB state transition node id which consumes this input.
+pub const PSBT_IN_RGB_CONSUMED_BY: u8 = 0x03;
 
 /// Extension trait for static functions returning RGB-related proprietary keys.
 pub trait ProprietaryKeyRgb {
@@ -51,11 +51,11 @@ pub trait ProprietaryKeyRgb {
         }
     }
 
-    /// Constructs [`PSBT_IN_RGB_NODE_ID`] proprietary key.
-    fn rgb_node_id(contract_id: ContractId) -> ProprietaryKey {
+    /// Constructs [`PSBT_IN_RGB_CONSUMED_BY`] proprietary key.
+    fn rgb_in_consumed_by(contract_id: ContractId) -> ProprietaryKey {
         ProprietaryKey {
             prefix: PSBT_RGB_PREFIX.to_vec(),
-            subtype: PSBT_IN_RGB_NODE_ID,
+            subtype: PSBT_IN_RGB_CONSUMED_BY,
             key: contract_id.to_bytes().to_vec(),
         }
     }
@@ -78,22 +78,56 @@ pub enum KeyError {
 
 pub trait RgbExt {
     fn has_rgb_contract(&self, contract_id: ContractId) -> bool {
-        self.rgb_contracts().contains(&contract_id)
+        self.rgb_contract_ids().contains(&contract_id)
     }
 
-    fn rgb_contracts(&self) -> BTreeSet<ContractId>;
+    fn rgb_contract_ids(&self) -> BTreeSet<ContractId>;
 
-    fn merge_rgb_transition(
+    fn rgb_contract(&self, contract_id: ContractId) -> Result<Option<Contract>, KeyError>;
+
+    fn rgb_contract_consumers(
         &self,
-        contract: Contract,
-        transition: Transition,
-    ) -> Result<bool, KeyError>;
+        contract_id: ContractId,
+    ) -> Result<BTreeSet<(NodeId, u16)>, KeyError>;
+
+    fn merge_rgb_contract(&self, contract: Contract) -> Result<bool, KeyError>;
+
+    fn rgb_node_ids(&self) -> BTreeSet<NodeId>;
+
+    fn rgb_transitions(&self) -> BTreeMap<NodeId, Transition> {
+        self.rgb_node_ids()
+            .into_iter()
+            .filter_map(|node_id| {
+                self.rgb_transition(node_id)
+                    .ok()
+                    .flatten()
+                    .map(|ts| (node_id, ts))
+            })
+            .collect()
+    }
+
+    fn rgb_transition(&self, node_id: NodeId) -> Result<Option<Transition>, KeyError>;
+
+    fn push_rgb_transition(&self, transition: &Transition) -> bool;
+
+    fn rgb_bundles(&self) -> Result<BTreeMap<ContractId, TransitionBundle>, KeyError> {
+        self.rgb_contract_ids()
+            .into_iter()
+            .map(|contract_id| {
+                let mut map: BTreeMap<NodeId, BTreeSet<u16>> = bmap!();
+                for (node_id, no) in self.rgb_contract_consumers(contract_id)? {
+                    map.entry(node_id).or_default().insert(no);
+                }
+                Ok((contract_id, TransitionBundle::from(map)))
+            })
+            .collect()
+    }
 }
 
 pub trait RgbInExt {
-    fn rgb_node(&self, contract_id: ContractId) -> Result<Option<NodeId>, KeyError>;
+    fn rgb_consumer(&self, contract_id: ContractId) -> Result<Option<NodeId>, KeyError>;
 
-    /// Adds information about state transition to the PSBT input.
+    /// Adds information about state transition consuming this PSBT input.
     ///
     /// # Returns
     ///
@@ -104,37 +138,32 @@ pub trait RgbInExt {
     ///
     /// If the input already contains [`PSBT_IN_RGB_NODE_ID`] key with the given `contract_id` but
     /// referencing different [`NodeId`].
-    fn add_rgb_node(&mut self, contract_id: ContractId, node_id: NodeId) -> Result<bool, KeyError>;
-}
-
-impl RgbExt for Psbt {
-    fn rgb_contracts(&self) -> BTreeSet<ContractId> { todo!() }
-
-    fn merge_rgb_transition(
-        &self,
-        contract: Contract,
-        transition: Transition,
-    ) -> Result<bool, KeyError> {
-        // DO not forget to merge contract
-        todo!()
-    }
+    fn set_rgb_consumer(
+        &mut self,
+        contract_id: ContractId,
+        node_id: NodeId,
+    ) -> Result<bool, KeyError>;
 }
 
 impl RgbInExt for psbt::Input {
-    fn rgb_node(&self, contract_id: ContractId) -> Result<Option<NodeId>, KeyError> {
+    fn rgb_consumer(&self, contract_id: ContractId) -> Result<Option<NodeId>, KeyError> {
         self.proprietary
-            .get(&ProprietaryKey::rgb_node_id(contract_id))
+            .get(&ProprietaryKey::rgb_in_consumed_by(contract_id))
             .map(Vec::as_slice)
             .map(NodeId::from_slice)
             .transpose()
             .map_err(KeyError::from)
     }
 
-    fn add_rgb_node(&mut self, contract_id: ContractId, node_id: NodeId) -> Result<bool, KeyError> {
-        match self.rgb_node(contract_id)? {
+    fn set_rgb_consumer(
+        &mut self,
+        contract_id: ContractId,
+        node_id: NodeId,
+    ) -> Result<bool, KeyError> {
+        match self.rgb_consumer(contract_id)? {
             None => {
                 self.proprietary.insert(
-                    ProprietaryKey::rgb_node_id(contract_id),
+                    ProprietaryKey::rgb_in_consumed_by(contract_id),
                     node_id.as_slice().to_vec(),
                 );
                 Ok(true)
