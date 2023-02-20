2554c1b152105cd112cd090ba6d9ee026ed187eb
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2023-02-20 16:09:20 +0100

    Start everything from scratch

--- src/core/contract.rs
+++
@@ -1,5 +0,0 @@
-/// Allocation map using unique set of seal definitions
-pub type SealValueMap = BTreeMap<seal::Revealed, ValueAtom>;
-
-/// Allocation map using unique set of blinded consignment endpoints
-pub type EndpointValueMap = BTreeMap<SealEndpoint, ValueAtom>;
