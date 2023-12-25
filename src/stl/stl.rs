34079cbe5447bdb13583b00a416de6579019bcaf
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-12-25 23:20:33 +0100

    release v0.11.0-beta.3

--- src/stl/stl.rs
+++ src/stl/stl.rs
@@ -44,7 +44,7 @@ pub const LIB_ID_RGB_CONTRACT: &str =
 
 /// Strict types id for the library representing of RGB StdLib data types.
 pub const LIB_ID_RGB_STD: &str =
-    "urn:ubideco:stl:2N7iehth1MBA1yQaJebjMxw45x9fGYAMNqrmGU3pxwvU#tribal-modem-andy";
+    "urn:ubideco:stl:FjdM8g7HN2S8hfKB17GuwsnKRzFM9up8bJ4uBNzWAPDL#aloha-olivia-rider";
 
 fn _rgb_std_stl() -> Result<TypeLib, CompileError> {
     LibBuilder::new(libname!(LIB_NAME_RGB_STD), tiny_bset! {
