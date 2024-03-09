8daa323203682cd6c8476d8d68782f5d687dabd7
jack <cymqqqq@gmail.com>
2024-03-09 20:45:23 +0800

    fix: delete test

--- src/test/inventory.rs
+++
@@ -1,9 +0,0 @@
-use crate::persistence::Inventory;
-use rgb::contract::{ContractId, XOutputSeal, XChain};
-use bp::seals::SecretSeal;
-
-pub fn test_transfer(
-    contract_id: ContractId,
-    outputs: impl AsRef<[XOutputSeal]>,
-    secret_seals: impl AsRef<[XChain<SecretSeal>]>,
-)
