aecd8ba13fbbf41568fa07db4467b15e7498c818
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2024-04-07 11:14:28 +0200

    chore: update dependencies and stl

--- src/stl/stl.rs
+++ src/stl/stl.rs
@@ -42,7 +42,7 @@ pub const LIB_ID_RGB_CONTRACT: &str =
 
 /// Strict types id for the library representing of RGB StdLib data types.
 pub const LIB_ID_RGB_STD: &str =
-    "urn:ubideco:stl:9ckm15QYX2T9qUMbqw3mQaVu5VwYNw5VRYj6WiMZDETw#alias-crystal-subject";
+    "urn:ubideco:stl:DpTMtUwcmoREFPCifvJRfz6VYafwVoswkxdyDGS6dJAj#subway-hostel-major";
 
 fn _rgb_std_stl() -> Result<TypeLib, CompileError> {
     LibBuilder::new(libname!(LIB_NAME_RGB_STD), tiny_bset! {
